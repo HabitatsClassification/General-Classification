e922ceb527d7d05cc8ce90564d9bf33200286b57
Luíz Fernando Esser <luizesser@gmail.com>
2020-08-23 22:43:45 -0300

    Able to run with different sets of algorithms
    Mudei algumas abordagens, permitindo que diferentes conjuntos de algoritmos sejam escolhidos, bem como diferentes limiares (argumento th da função classification)

--- Habitats_Classification.R
+++ Habitats_Classification.R
@@ -2,6 +2,7 @@ library(caretEnsemble)
 library(caret)
 library(dplyr)
 library(here)
+library(pROC)
 
 #### Create/Open Data ####
 algo <- c("kknn","rf", "LogitBoost", "nnet", "svmRadial")
@@ -24,73 +25,64 @@ levels(df$vegetation.type) <- gsub("Subtropical riverine semideciduous forest" ,
 levels(df$vegetation.type) <- gsub("Tropical riverine semideciduous forest"    ,"Riverine",       levels(df$vegetation.type))
 head(df)
 
-classification <- function(df){
-  nr <- createDataPartition(df$vegetation.type, p=0.9, list=FALSE)
+classification <- function(df, th){
+  habitats <- levels(df[,1])
+  nr <- createDataPartition(df[,1], p=0.9, list=FALSE)
   train <- df[nr,]
   test <- df[-nr,]
   #### Generatind Methods ####
-  multiclass <- function(train, test, algo, th=0.6){
+  multiclass <- function(train, test, algo){
     t <- trainControl(
         method = "repeatedcv", 
         number = 10, 
-        index = createFolds(train$vegetation.type, 10),
+        index = createFolds(train[,1], 10),
         repeats = 10, 
         savePredictions = 'final', 
         classProbs = TRUE, 
         summaryFunction = multiClassSummary,
         allowParallel = F, 
         sampling = "up", 
-        returnResamp = "final"
+        returnResamp = "final",
+        selectionFunction = "best"
       )
     ensemble <- caretList(
-        vegetation.type ~ ., 
+        vegetation.type ~ ., # transformar "vegetation.type" em algo mais genérico
         data=train, 
         trControl=t, 
         methodList=algo, 
-        continue_on_fail=T
+        continue_on_fail=T,
+        metric="AUC"
       )
     model_preds <- lapply(ensemble, predict, newdata=test, type="prob")
     
-    auc_knn  <- ensemble$kknn$results$AUC[ensemble$kknn$results$kmax == ensemble$kknn$bestTune$kmax]
-    auc_rf   <- ensemble$rf$results$AUC[ensemble$rf$results$mtry == ensemble$rf$bestTune[1,]]
-    auc_log  <- ensemble$LogitBoost$results$AUC[ensemble$LogitBoost$results$nIter == ensemble$LogitBoost$bestTune[1,]]
-    auc_nnet <- ensemble$nnet$results$AUC[ensemble$nnet$results$size == ensemble$nnet$bestTune[1,1] & 
-                                            ensemble$nnet$results$decay == ensemble$nnet$bestTune[1,2]]
-    auc_svm  <- ensemble$svmRadial$results$AUC[ensemble$svmRadial$results$sigma == ensemble$svmRadial$bestTune[1,1] &
-                                                 ensemble$svmRadial$results$C == ensemble$svmRadial$bestTune[1,2] ]
-    
-    if(auc_knn  < th){ auc_knn  <- 0}
-    if(auc_rf   < th){ auc_rf   <- 0}
-    if(auc_log  < th){ auc_log  <- 0}
-    if(auc_nnet < th){ auc_nnet <- 0}
-    if(auc_svm  < th){ auc_svm  <- 0}
-    auc_sum <- auc_knn + auc_rf + auc_log + auc_nnet + auc_svm 
-    
-    Restinga_wmean <- (model_preds$kknn$Restinga*auc_knn + model_preds$svm$Restinga*auc_svm +
-                         model_preds$rf$Restinga*auc_rf + model_preds$LogitBoost$Restinga*auc_log +   
-                         model_preds$nnet$Restinga*auc_rf) / auc_sum
-    Rainforest_wmean <- (model_preds$kknn$Rainforest*auc_knn + model_preds$svm$Rainforest*auc_svm +
-                           model_preds$rf$Rainforest*auc_rf + model_preds$LogitBoost$Rainforest*auc_log +   
-                           model_preds$nnet$Rainforest*auc_rf) / auc_sum
-    Semideciduous_wmean <- (model_preds$kknn$Semideciduous*auc_knn + model_preds$svm$Semideciduous*auc_svm +
-                              model_preds$rf$Semideciduous*auc_rf + model_preds$LogitBoost$Semideciduous*auc_log +   
-                              model_preds$nnet$Semideciduous*auc_rf) / auc_sum
-    High.Elevation_wmean <- (model_preds$kknn$High.Elevation*auc_knn + model_preds$svm$High.Elevation*auc_svm +
-                               model_preds$rf$High.Elevation*auc_rf + model_preds$LogitBoost$High.Elevation*auc_log +   
-                               model_preds$nnet$High.Elevation*auc_rf) / auc_sum
-    Rocky_wmean <- (model_preds$kknn$Rocky*auc_knn + model_preds$svm$Rocky*auc_svm +
-                      model_preds$rf$Rocky*auc_rf + model_preds$LogitBoost$Rocky*auc_log +   
-                      model_preds$nnet$Rocky*auc_rf) / auc_sum
-    Riverine_wmean <- (model_preds$kknn$Riverine*auc_knn + model_preds$svm$Riverine*auc_svm +
-                         model_preds$rf$Riverine*auc_rf + model_preds$LogitBoost$Riverine*auc_log +   
-                         model_preds$nnet$Riverine*auc_rf) / auc_sum
-    result <- cbind(test, Rainforest_wmean, Semideciduous_wmean, High.Elevation_wmean, Restinga_wmean, Riverine_wmean, Rocky_wmean)
-    final_prediction <- apply(result[,-(1:4)],1,function(x) gsub("_wmean","",names(which.max(x))) )
-    roc_probs <- NA
-    for(i in 1:nrow(result)){
-      roc_probs[i] <- result[i,colnames(result) == paste0(result$vegetation.type, "_wmean")[i]]
+    #oneSE(ensemble[[1]]$results, "AUC", num = 10, maximize = T)
+    values_auc <- NA
+    for (i in 1:length(algo)) {
+      v <- ensemble[[i]]$results$AUC[best(ensemble[[i]]$results, "AUC", maximize = T)]
+      if(v > th){
+        assign(paste0("auc_",algo[i]), v)
+        values_auc[i] <- v
+      } else {
+        assign(paste0("auc_",algo[i]), 0)
+        values_auc[i] <- 0
+      }
     }
-    result <- cbind(result, final_prediction, roc_probs)
+    sum_auc <- sum(values_auc)
+    result <- test
+    for (i in 1:length(habitats)){
+      df2 <- sapply(model_preds,'[',i)
+      df2 <- Map('*',df2,values_auc)
+      df2 <- as.data.frame(df2)
+      df2 <- rowSums(df2)/sum_auc
+      result <- cbind(result,df2)
+    }
+    colnames(result) <- c(colnames(test), habitats)
+    final_prediction <- apply(result[,-(1:4)],1,function(x) names(which.max(x)))
+    habitat_probs <- NA
+    for(i in 1:nrow(result)){
+      habitat_probs[i] <- result[i,colnames(result) == result[,1][i]]
+    }
+    result <- cbind(result, final_prediction, habitat_probs)
     return(result)
   }
   one_vs_all <- function(train, test, algo){
@@ -113,15 +105,27 @@ classification <- function(df){
     Rocky$vegetation.type <- ifelse(Rocky$vegetation.type != "Rocky", "Other", "Rocky") %>% 
       factor(levels = c("Rocky", "Other"))
     
-    habitat_classification <- function(df, algo){
+    habitat_classification <- function(df, algo, th=0.6){
       nr <- createDataPartition(df$vegetation.type, p=0.9, list=FALSE)
       train2 <- df[nr,]
       u <- unique(df$vegetation.type)
       u <- u[u != "Other"]
-      t <- trainControl(method = "repeatedcv", number = 10, index = createFolds(train2$vegetation.type, 10),
-                        repeats = 10, savePredictions = 'final', classProbs = TRUE, summaryFunction = twoClassSummary,
-                        allowParallel = F, sampling = "down")
-      ensemble <-  caretList(vegetation.type ~ ., data=train2, trControl=t, methodList=algo, continue_on_fail=T,  verbose=T)
+      t <- trainControl(method = "repeatedcv", 
+                        number = 10, 
+                        index = createFolds(train2$vegetation.type, 10),
+                        repeats = 10, 
+                        savePredictions = 'final', 
+                        classProbs = TRUE, 
+                        summaryFunction = twoClassSummary,
+                        allowParallel = F, 
+                        sampling = "down")
+      ensemble <-  caretList(vegetation.type ~ ., 
+                             data=train2, 
+                             trControl=t, 
+                             methodList=algo, 
+                             continue_on_fail=T,
+                             metric="ROC",
+                             verbose=T)
       assign(paste0(u,"_",algo[1]), ensemble)
     }
     
@@ -139,53 +143,55 @@ classification <- function(df){
                    "Restinga_result",
                    "Semideciduous_result",
                    "High.Elevation_result")
+    ensembles2 <- c("Rainforest",
+                    "Riverine",
+                    "Rocky",
+                    "Restinga",
+                    "Semideciduous",
+                    "High.Elevation")
+    result <- test
     
     for (j in 1:length(ensembles)){
       print(j)
       en <- get(ensembles[j])
       
-      ROC_knn  <- en$kknn$results$ROC[en$kknn$results$kmax == en$kknn$bestTune$kmax]
-      ROC_rf   <- en$rf$results$ROC[en$rf$results$mtry == en$rf$bestTune[1,]]
-      ROC_log  <- en$LogitBoost$results$ROC[en$LogitBoost$results$nIter == en$LogitBoost$bestTune[1,]]
-      ROC_nnet <- en$nnet$results$ROC[en$nnet$results$size == en$nnet$bestTune[1,1] & 
-                                        en$nnet$results$decay == en$nnet$bestTune[1,2]]
-      ROC_svm  <- en$svmRadial$results$ROC[en$svmRadial$results$sigma == en$svmRadial$bestTune[1,1] &
-                                             en$svmRadial$results$C == en$svmRadial$bestTune[1,2] ]
-      
-      if(ROC_knn  < 0.6){ ROC_knn  <- 0}
-      if(ROC_rf   < 0.6){ ROC_rf   <- 0}
-      if(ROC_log  < 0.6){ ROC_log  <- 0}
-      if(ROC_nnet < 0.6){ ROC_nnet <- 0}
-      if(ROC_svm  < 0.6){ ROC_svm  <- 0}
-      
-      ROC_sum <- ROC_knn + ROC_rf + ROC_log + ROC_nnet + ROC_svm 
-      
+      values_roc <- NA
+      for (i in 1:length(algo)) {
+        v <- en[[i]]$results$ROC[best(en[[i]]$results, "ROC", maximize = T)]
+        if(v > th){
+          assign(paste0("roc_",algo[i]), v)
+          values_roc[i] <- v
+        } else {
+          assign(paste0("roc_",algo[i]), 0)
+          values_roc[i] <- 0
+        }
+      }
+    
+      sum_roc <- sum(values_roc)
       model_preds <- lapply(en, predict, newdata=test, type="prob")
-      
-      assign(paste0(gsub("_result", "_wmean", ensembles[j])), 
-             (model_preds$kknn[model_preds$kknn != "Other"]*ROC_knn +
-                model_preds$svmRadial[model_preds$svmRadial != "Other"]*ROC_svm +
-                model_preds$rf[model_preds$rf != "Other"]*ROC_rf +
-                model_preds$LogitBoost[model_preds$LogitBoost != "Other"]*ROC_log +  
-                model_preds$nnet[model_preds$nnet != "Other"]*ROC_nnet) / ROC_sum )
+      df2 <- sapply(model_preds,'[',ensembles2[j])
+      df2 <- Map('*',df2,values_roc)
+      df2 <- as.data.frame(df2)
+      df2 <- rowSums(df2)/sum_roc
+      result <- cbind(result,df2)
     }
     
-    result <- cbind(test, Rainforest_wmean, Semideciduous_wmean, High.Elevation_wmean, Restinga_wmean, Riverine_wmean, Rocky_wmean)
+    colnames(result) <- c(colnames(test), ensembles2)
+    
     
     final_prediction <- colnames(result[,-(1:4)])[apply(result[,-(1:4)],1,which.max)]
-    final_prediction <- gsub("_wmean", "", final_prediction)
-    auc_probs <- NA
+    habitat_probs <- NA
     for(i in 1:nrow(result)){
-      auc_probs[i] <- result[i,colnames(result) == paste0(result$vegetation.type, "_wmean")[i]]
+      habitat_probs[i] <- result[i,colnames(result) == result[,1][i]]
     }
-    result <- cbind(result, final_prediction, auc_probs)
+    result <- cbind(result, final_prediction, habitat_probs)
     return(result)
   }
   result_multiclass <- multiclass(train, test, algo)
   result_one_vs_all <- one_vs_all(train, test, algo)
   #### Comparing Methods ####
-  mc.roc <- multiclass.roc(result_multiclass$vegetation.type, result_multiclass$roc_probs)
-  oa.roc <- multiclass.roc(result_one_vs_all$vegetation.type, result_one_vs_all$auc_probs)
+  mc.roc <- multiclass.roc(result_multiclass[,1], result_multiclass$habitat_probs)
+  oa.roc <- multiclass.roc(result_one_vs_all[,1], result_one_vs_all$habitat_probs)
   result <- list("Multiclass" = mc.roc, "One.vs.all" = oa.roc)
   return(result)
 }
