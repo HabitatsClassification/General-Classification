97e82d078adea848ceb7cc6b60b46f82502613b5
Luíz Fernando Esser <luizesser@gmail.com>
2020-10-07 21:51:35 -0300

    Update train/test
    separação de treino e teste dentro da função, gerando novo set a cada volta.

--- Habitats_Classification2.R
+++ Habitats_Classification2.R
@@ -21,48 +21,107 @@ levels(df$vegetation.type) <- gsub("Subtropical riverine semideciduous forest" ,
 levels(df$vegetation.type) <- gsub("Tropical riverine semideciduous forest"    ,"Riverine",       levels(df$vegetation.type))
 head(df)
 
-## to normalize data: (não demonstrou resultados muito melhores)
-#df2 <- t(apply(df[-1], 1, prop.table))
-#df <- cbind(df, df2)
-#df <- df[,-(2:4)]
+habitats_training_1 <- function(n){
+  models <- NA
+  final_aucs <- NA
+  all_aucs <- list()
+  all_models <- list()
+  all_best_tunes <- list()
+  train_sets <- list()
+  test_sets <- list()
+  for (j in 1:n){
+    # Clustering
+    nr <- createDataPartition(df[,1], p=0.9, list=FALSE)
+    train <- df[nr,]
+    test <- df[-nr,]
+    #train = 50 clusters
+    rain <- train[train$vegetation.type == "Rainforest",-1]
+    rest <- train[train$vegetation.type == "Restinga",-1]
+    high <- train[train$vegetation.type == "High.Elevation",-1]
+    semi <- train[train$vegetation.type == "Semideciduous",-1]
+    rive <- train[train$vegetation.type == "Riverine",-1]
+    rock <- train[train$vegetation.type == "Rocky",-1]
+    rain <- kmeans(rain, 50)
+    rest <- kmeans(rest, 50)
+    high <- kmeans(high, 50)
+    semi <- kmeans(semi, 50)
+    rive <- kmeans(rive, 50)
+    rock <- kmeans(rock, 50)
+    rain <- rain$centers
+    rest <- rest$centers
+    high <- high$centers
+    semi <- semi$centers
+    rive <- rive$centers
+    rock <- rock$centers
+    vegetation.type <- c(rep("Rainforest",     50), 
+                         rep("Semideciduous",  50), 
+                         rep("High.Elevation", 50), 
+                         rep("Restinga",       50), 
+                         rep("Riverine",       50), 
+                         rep("Rocky",          50))
+    families <- as.data.frame(rbind(rain, semi, high, rest, rive, rock))
+    train <- data.frame(vegetation.type = as.character(vegetation.type),
+                        myrtaceae = as.numeric(families$myrtaceae),
+                        fabaceae  = as.numeric(families$fabaceae),
+                        rubiaceae = as.numeric(families$rubiaceae))
+    train_sets[[j]] <- train
+    test_sets[[j]] <- test
+    # apply function 
+    ensemble <- caretList(reformulate(termlabels = colnames(train)[-1], response=colnames(train)[1]),
+                          data=train,
+                          trControl=trainControl(
+                            method = "repeatedcv", 
+                            number = 10, 
+                            index = createFolds(train[,1], 10, returnTrain = T),
+                            repeats = 10, 
+                            savePredictions = 'final', 
+                            classProbs = T, 
+                            summaryFunction = multiClassSummary,
+                            allowParallel = F, 
+                            returnResamp = "final",
+                            selectionFunction = "best"), 
+                          tuneList = list(
+                            naive_bayes = caretModelSpec(method = "naive_bayes", 
+                                                         tuneGrid = expand.grid(laplace = c(0),
+                                                                                usekernel = TRUE,
+                                                                                adjust = seq(0.65, 0.75, 0.01))),
+                            nnet = caretModelSpec(method = "nnet", 
+                                                  tuneGrid = expand.grid(size = seq(10, 15, 1), # 10, 30
+                                                                         decay = seq(0.6,0.8,0.1))), # 0.65, 0.8, 0.05
+                            mlp = caretModelSpec(method = "mlp", 
+                                                 tuneGrid = expand.grid(size = seq(10, 20, 1)))),
+                          continue_on_fail=F,
+                          metric="AUC")
+    model_preds <- lapply(ensemble, predict.train, newdata=test, type="raw")
+    x <- lapply(lapply(model_preds, as.numeric), multiclass.roc, response = as.numeric(test$vegetation.type))
+    preds_auc <- NA
+    for (i in 1:length(names(ensemble))){
+      preds_auc[i] <- as.numeric(x[[i]]$auc)
+    }
+    models[j] <- ensemble[which.max(preds_auc)]
+    final_aucs[j] <- preds_auc[which.max(preds_auc)]
+    all_aucs[[j]] <- preds_auc
+    all_models[[j]] <- ensemble
+    best_tunes <- list()
+    for (k in 1:length(names(ensemble))){
+      best_tunes[[k]] <- ensemble[[k]]$bestTune
+    }
+    all_best_tunes[[j]] <- best_tunes
+  } 
+  best_model <- models[which.max(final_aucs)]
+  
+  return(list(Models = models, 
+              AUCs = final_aucs, 
+              Best_Model = best_model, 
+              All_Best_Tunes = all_best_tunes,
+              All_Models = all_models,
+              All_AUCs = all_aucs,
+              Train_sets = train_sets, 
+              Test_sets = test_sets))
+}
 
-######### Clustering
-nr <- createDataPartition(df[,1], p=0.9, list=FALSE)
-train <- df[nr,]
-test <- df[-nr,]
+test_1 <- habitats_training(4)
 
-#train = clusters
-table(train$vegetation.type)
-
-rain1 <- train[train$vegetation.type == "Rainforest",-1]
-rest1 <- train[train$vegetation.type == "Restinga",-1]
-high1 <- train[train$vegetation.type == "High.Elevation",-1]
-semi1 <- train[train$vegetation.type == "Semideciduous",-1]
-rive1 <- train[train$vegetation.type == "Riverine",-1]
-
-rain2 <- kmeans(rain1, 65)
-rest2 <- kmeans(rest1, 65)
-high2 <- kmeans(high1, 65)
-semi2 <- kmeans(semi1, 65)
-rive2 <- kmeans(rive1, 65)
-
-rain3 <- rain2$centers
-rest3 <- rest2$centers
-high3 <- high2$centers
-semi3 <- semi2$centers
-rive3 <- rive2$centers
-
-vegetation.type <- c(rep("Rainforest", 65), 
-                     rep("Semideciduous", 65), 
-                     rep("High.Elevation", 65), 
-                     rep("Restinga", 65), 
-                     rep("Riverine", 65), 
-                     rep("Rocky", 65))
-train <- cbind(vegetation.type, rbind(rain3, semi3, high3, rest3, rive3, train[train$vegetation.type == "Rocky",-1]))
-habitats <- levels(train[,1])
-table(train$vegetation.type)
-table(test$vegetation.type)
-######### 
 
 # visualyze clusters:
 # library("factoextra")
@@ -88,60 +147,4 @@ table(test$vegetation.type)
 #             legend.title = "Diagnosis") +
 #  ggtitle("2D PCA-plot from 30 feature dataset") +
 #  theme(plot.title = element_text(hjust = 0.5))
-
-habitats_training <- function(n){
-  models <- NA
-  final_aucs <- NA
-  for (j in 1:n){
-    ensemble <- caretList(reformulate(termlabels = colnames(train)[-1], response=colnames(train)[1]),
-                          data=train,
-                          trControl=trainControl(
-                            method = "repeatedcv", 
-                            number = 10, 
-                            index = createFolds(train[,1], 10, returnTrain = T),
-                            repeats = 10, 
-                            savePredictions = 'final', 
-                            classProbs = TRUE, 
-                            summaryFunction = multiClassSummary,
-                            preProcOptions=list(k=65),
-                            allowParallel = T, 
-                            returnResamp = "final",
-                            selectionFunction = "best"), 
-                          tuneList = list(
-                            naive_bayes = caretModelSpec(method = "naive_bayes", 
-                                                         tuneGrid = expand.grid(laplace = c(0),
-                                                                                usekernel = TRUE,
-                                                                                adjust = seq(0.65, 0.75, 0.01))),
-                            nnet = caretModelSpec(method = "nnet", 
-                                                  tuneGrid = expand.grid(size = seq(10, 15, 1), # 10, 30
-                                                                         decay = seq(0.6,0.8,0.1))), # 0.65, 0.8, 0.05
-                            mlp = caretModelSpec(method = "mlp", 
-                                                 tuneGrid = expand.grid(size = seq(10, 20, 1)))),
-                          continue_on_fail=T,
-                          metric="AUC")
-    model_preds <- lapply(ensemble, predict, newdata=test, type="raw")
-    x <- lapply(lapply(model_preds, as.numeric), multiclass.roc, response = as.numeric(test$vegetation.type))
-    preds_auc <- NA
-    for (i in 1:length(names(ensemble))){
-      preds_auc[i] <- as.numeric(x[[i]]$auc)
-    }
-    models[j] <- ensemble[which.max(preds_auc)]
-    final_aucs[j] <- preds_auc[which.max(preds_auc)]
-  } 
-  best_model <- models[which.max(final_aucs)]
-  return(list(Models = models, AUCs = final_aucs, Best_model = best_model))
-}
-  
-  
-test_1 <- habitats_training(1)
-test_2 <- habitats_training(2)
-test_3 <- habitats_training(3)
-test_4 <- habitats_training(4)
-
-
-  
-  
-  
-  
-  
   
